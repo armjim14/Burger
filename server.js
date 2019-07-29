d5912e343447b4b1b4d7f41bbca245685c7091e5
armjim14 <ajimenez1497@gmail.com>
2019-07-28 20:00:35 -0700

    added styling and a each with if/else for index.html

--- server.js
+++ server.js
@@ -1,22 +1,43 @@
 var express = require("express");
 var exh = require("express-handlebars");
+var mysql = require("mysql");
 
 var app = express();
+
+// var conn = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     database: "restaurants",
+//     password: ""
+// })
+
+// conn.connect();
 
 app.engine("handlebars", exh({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var fav = [
+// app.get("/", (req, res) => {
+//     conn.query(`SELECT * FROM food`, (err, data) => {
+//         res.render("index", {send: data});
+//     })
+// })
+
+var test = [
     {
-    name: "what"
+        id: 1,
+        name: "bacon burger",
+        done: false
     },
     {
-    name: "no"
+        id: 2,
+        name: "double burger",
+        done: true
     }
 ]
 
-app.get("/test", (req, res) => {
-    res.render("index", {send: fav});
+app.get("/", (req, res) => {
+    res.render("index", {send: test})
 })
 
 app.listen(3000, () => {
