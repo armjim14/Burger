36597e7e0a332389f169b47a504e7ae8860730c1
armjim14 <ajimenez1497@gmail.com>
2019-08-04 17:31:36 -0700

    inital heroku commit

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 var mysql = require("mysql");
+var conn;
 
-var conn = mysql.createConnection({
-    host: "localhost",
-    port: 0000,
-    user: "root",
-    database: "restaurants",
-    password: ""
-})
+if ( process.env.JAWSDB_URL ){
+    conn = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var conn = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        database: "restaurants",
+        password: "rootroot"
+    })
+}
 
 conn.connect();
 
