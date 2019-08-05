89666f24dabe4487aa45710f67bbbe28e7621664
armjim14 <ajimenez1497@gmail.com>
2019-08-05 00:40:27 -0700

    changed mysql info

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if ( process.env.JAWSDB_URL ){
     conn = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var conn = mysql.createConnection({
-        host: "localhost",
+        host: "o3iyl77734b9n3tg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "root",
-        database: "restaurants",
-        password: "rootroot"
+        user: "fkfkwm9nkv9p03gl",
+        database: "ji8cc56wxea75wzz",
+        password: "f2t3mkp29gplveip"
     })
 }
 
